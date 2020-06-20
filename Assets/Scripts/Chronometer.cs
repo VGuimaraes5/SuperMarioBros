772a6cc2cfa7be5ec1e23e2e6951aeecfe70d344
Vinicius Guimarães <vinicius.guimaraes5@fatec.sp.gov.br>
2020-06-20 18:32:38 -0300

    Add UI

---
+++ Assets/Scripts/Chronometer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chronometer : MonoBehaviour
+{
+    public float m_MaxTime = 300.0f;
+    public float m_ElapsedTime = 0.0f;
+    public bool m_FinishLevel = false;
+    public string m_Mask = "000";
+    private Text m_Text;
+
+    private void Start()
+    {
+        m_Text = GetComponent<Text>();
+        UpdateUI();
+    }
+
+    private void Update()
+    {
+        if (m_FinishLevel)
+            return;
+
+        m_ElapsedTime += Time.deltaTime;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        m_Text.text = Mathf.Ceil(m_MaxTime - m_ElapsedTime).ToString(m_Mask);
+    }
+}
---
+++ Assets/Scripts/MastScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MastScript : MonoBehaviour
+{
+    public Rigidbody m_Flag;
+    private bool m_TookFlag = false;
+    private float m_FlagVelocity = 5.0f;
+
+    private void Start()
+    {
+        m_Flag = GameObject.Find("Flag").GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (m_TookFlag)
+        {
+            MoveFlag();
+        }
+    }
+
+    private void MoveFlag()
+    {
+        if (m_Flag.position.y > -1.9f)
+        {
+            Vector3 position = new Vector3(0.0f, m_FlagVelocity * Time.fixedDeltaTime, 0.0f);
+            m_Flag.MovePosition(m_Flag.position - position);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "PlayerTag")
+        {
+            m_TookFlag = true;
+        }
+    }
+}
